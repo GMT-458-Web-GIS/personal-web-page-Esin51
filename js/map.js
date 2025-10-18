2ce7572708c298de2cfc70dbc4673b44e435ea37
Esin51 <esinnguclu@hotmail.com>
2025-10-18 14:45:58 +0300

    İlk sürüm: 3 sayfa + CSS + OpenLayers harita

---
+++ js/map.js
@@ -0,0 +1,16 @@
+// OpenLayers: OSM zemin, Ankara merkezli
+const map = new ol.Map({
+    target: 'map',
+    layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }) ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([32.8597, 39.9334]),
+      zoom: 10
+    })
+  });
+  
+  // İsteğe bağlı mini etkileşim: tıklanan koordinatı konsola yaz
+  map.on('click', (evt) => {
+    const [lon, lat] = ol.proj.toLonLat(evt.coordinate);
+    console.log(`Koordinat: ${lon.toFixed(5)}, ${lat.toFixed(5)}`);
+  });
+  
